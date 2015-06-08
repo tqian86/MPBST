d5c7eae236236bf847341831a5ab7e25c1b7344f
Ting Qian <tqian86@gmail.com>
2015-06-08 17:16:47 -0400

    Corrected the previous algorithm. Fixed effects complete.

--- regression/lmm.py
+++ regression/lmm.py
@@ -21,7 +21,7 @@ class LMMSampler(RegressionSampler):
         self.group_predictors = {}
         self.group_values = {}
         self.predictor_groups = {}
-        self.params = pd.DataFrame(None, index = range(self.niter), columns = [])
+        self.params = pd.DataFrame(1.0, index = range(self.niter), columns = ['sigma2'])
         
     def set_fixed(self, outcome, predictors):
         """Set the regression model in terms of a univariate outcome
@@ -59,59 +59,43 @@ class LMMSampler(RegressionSampler):
                 except: self.predictor_groups[predictor] = [group_name]
                 # create parameter storage
                 for gv in group_values:
-                    self.params.loc[:,'beta_{0}_{1}'.format(predictor, gv)] = 0
+                    self.params.loc[:,'beta_{0}_{1}{2}'.format(predictor, group_name, gv)] = 0
+                self.params.loc[:,'sigma2_{0}_{1}'.format(predictor, group_name)] = 1.0
         return
     
     def do_inference(self, output_file=None):
-        for i in xrange(1, self.niter):
-            self.infer_fixed(iter = i, output_file = output_file)
-        return
+        for iter in xrange(1, self.niter):
 
-    def infer_fixed(self, iter, output_file=None):
-        """Infer the fixed effects.
-        """
-        for p in self.predictors:
-            # check if p has random effects
-            if p in self.predictor_groups:
-                self._infer_fixed_w_random(p, iter, output_file)
-            else:
-                self._infer_fixed_wo_random(p, iter, output_file)
+            # Infer the fixed effects.
+            for p in self.predictors:
+                self._infer_fixed_beta(p, iter, output_file)
+                
+            # Infer overall model variance sigma2
+            self._infer_sigma2(iter, output_file = None)
 
-    def _infer_fixed_wo_random(self, p, iter, output_file = None):
-        """Infer the beta coefficient of a fixed effect that has no random effects.
-        """
-        return
-
-    def _infer_fixed_w_random(self, p, iter, output_file = None):
+    def _infer_fixed_beta(self, p, iter, output_file = None):
         """Infer the beta coefficient of a fixed effect that has random effects.
         """
-        proposal_sd = .1
+        proposal_sd = .2
         old_beta = self.params.loc[iter-1, 'beta_%s' % p]
         new_beta = random.gauss(mu = old_beta, sigma = proposal_sd)
 
         # set up to calculate the g densities for both the old and new beta values
         log_g_old = 0 #-1 * old_beta # which is np.log(np.exp(-1 * old_beta))
         log_g_new = 0 #-1 * new_beta # similar as above
+        
+        Beta = self.params.filter(regex = '^beta').loc[iter-1]
+        X = self.obs.filter(items=[colname.split('_')[1] for colname in Beta.index])
 
-        k_0, mu_0, alpha_0, beta_0 = 0.1, 0, 1, 1
+        # calculate the old loglikelihood
+        log_g_old += - (1 / (2 * self.params.loc[iter-1, 'sigma2'])) * \
+                     np.sum((self.obs[self.outcome] - np.dot(X, Beta)) ** 2)
 
-        # retrieve all groups where this predictor has random effects
-        for group in self.predictor_groups[p]:
-            random_betas = self.params.loc[iter-1, ['beta_%s_%s' % (p, gv) for gv in self.group_values[group]]]
+        # modify the beta value and calculate the new loglikelihood
+        Beta.loc['beta_%s' % p] = new_beta
+        log_g_new += - (1 / (2 * self.params.loc[iter-1, 'sigma2'])) * \
+                     np.sum((self.obs[self.outcome] - np.dot(X, Beta)) ** 2)
 
-            x_bar, x_var = random_betas.mean(), random_betas.var()
-            n = len(random_betas)
-            k_n = k_0 + n
-            mu_n = (k_0 * mu_0 + n * x_bar) / k_n
-            alpha_n = alpha_0 + n / 2
-            beta_n = beta_0 + 0.5 * x_var * n + k_0 * n * (x_bar - mu_0) ** 2 / (2 * k_n)
-            Lambda = alpha_n * k_n / beta_n
-            
-            t_frozen = t(df = 2 * alpha_n, loc = mu_n, scale = (1 / Lambda) ** 0.5)
-            
-            log_g_old += t_frozen.logpdf(old_beta)
-            log_g_new += t_frozen.logpdf(new_beta)
-            
         # compute candidate densities q for old and new beta
         # since the proposal distribution is normal this step is not needed
         log_q_old = 0#np.log(dnorm(old_beta, loc = new_beta, scale = proposal_sd))
@@ -128,10 +112,23 @@ class LMMSampler(RegressionSampler):
             self.params.loc[iter, 'beta_%s' % p] = old_beta
             return False
                                     
-    
+
+    def _infer_sigma2(self, iter, output_file = None):
+        """Infer the variance of the overall multi-level model.
+        """
+        alpha_0 = 1
+        beta_0 = 1
+        alpha_n = alpha_0 + self.N / 2
+
+        Beta = self.params.filter(regex = '^beta').loc[iter-1]
+        X = self.obs.filter(items=[colname.split('_')[1] for colname in Beta.index])
+
+        beta_n = beta_0 + 0.5 * np.sum((self.obs[self.outcome] - np.dot(X, Beta)) ** 2)
+        self.params.loc[iter, 'sigma2'] = np.random.gamma(alpha_n, 1 / beta_n)
+        
 lmm = LMMSampler(record_best = True, cl_mode = False)
 lmm.read_csv('./data/10group-100n.csv')
 lmm.set_fixed(outcome = 'y', predictors = [1, 'x1', 'x2'])
 lmm.set_random(ranef_dict = {'group': [1, 'x1']})
 lmm.do_inference()
-print(lmm.params['beta_1'])
+print(lmm.params.iloc[900:1000])
