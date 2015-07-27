982cd2827f9bd435f48ce5dfd16bd18f21d1ad58
Ting Qian <tqian86@gmail.com>
2015-07-27 13:52:55 -0400

    Fixed a problem where log was not applied

--- clustering/hmm.pyx
+++ clustering/hmm.pyx
@@ -513,7 +513,7 @@ class GaussianHMMSampler(HMMSampler):
             joint_logp = np.empty(self.N, dtype=np.float32)
             
             # calculate transition probabilities first
-            joint_logp[0] = trans_p[0, states[0]]
+            joint_logp[0] = np.log(trans_p[0, states[0]])
             joint_logp[1:] = np.log(trans_p[states[:self.N-1], states[1:]])
 
             # then emission probs
