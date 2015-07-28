eced3d30708e45e86bb701d86941537aa6c77d42
Ting Qian <tqian86@gmail.com>
2015-07-28 13:45:50 -0400

    Updated the code to be compatible with latest clsampler

--- clustering/tests/hmm_test.py
+++ clustering/tests/hmm_test.py
@@ -4,7 +4,7 @@ from __future__ import print_function, division
 from MPBST.clustering.hmm import *
 
 if __name__ == '__main__':
-    hs = GaussianHMMSampler(num_states = 2, sample_size = 2000, record_best = True, cl_mode=False, debug_mumble = True)
+    hs = GaussianHMMSampler(num_states = 2, sample_size = 2000, search = True, cl_mode=False, debug_mumble = True, search_tolerance = 250)
     hs.read_csv('../toydata/speed.csv.gz', obs_vars = ['rt'])#, group = 'corr')
     gt, tt = hs.do_inference()
     print(gt, tt)
